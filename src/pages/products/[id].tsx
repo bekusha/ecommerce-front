3e62854908bc2dd3d85640f8a86f7a9f649e356b
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2024-03-13 12:35:58 +0400

    adding swiper/slider for images in productDetail page

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,15 +1,26 @@
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import axios from "axios";
 import React from "react";
-import { useRouter } from "next/router";
 import { Product } from "@/types/product";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
 
 interface ProductDetailProps {
   product: Product;
 }
 
 const ProductDetail: React.FC<ProductDetailProps> = ({ product }) => {
-  const router = useRouter();
+  const images = [
+    product.image1,
+    product.image2,
+    product.image3,
+    product.image4,
+    product.image5,
+  ].filter(Boolean);
 
   if (!product) {
     return <div>Loading...</div>;
@@ -17,9 +28,36 @@ const ProductDetail: React.FC<ProductDetailProps> = ({ product }) => {
 
   return (
     <div className="product-detail">
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <p>Price: ${product.price}</p>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            {image && (
+              <div
+                style={{
+                  position: "relative",
+                  width: "100%",
+                  height: "500px",
+                }}>
+                <Image
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div>{product.name}</div>
+      <div>{product.description}</div>
+      <div>{product.price} $</div>
+      <button>Add To Cart</button>
     </div>
   );
 };
