f8302c7bc4a92052f4c64a21d0f943193ecffd9b
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2024-04-21 21:11:54 +0400

    adding about and contact page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,26 +15,26 @@ interface AboutProps {
 const About = ({ data }: AboutProps) => {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-50 text-gray-800">
-      {/* <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
-      <p className="text-xl">{data.content}</p> */}
+      <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
+      <p className="text-xl">{data.content}</p>
     </div>
   );
 };
 
-// export async function getStaticProps() {
-//   try {
-//     const res = await axios.get(
-//       `${process.env.NEXT_PUBLIC_API_BASE_URL}content/about/`
-//     );
-//     return { props: { data: res.data } };
-//   } catch (error) {
-//     console.error("Error fetching about data:", error);
-//     // Consider a more sophisticated error handling strategy here
-//     return {
-//       props: {
-//         data: { title: "Error", content: "Could not load the content." },
-//       },
-//     };
-//   }
-// }
+export async function getStaticProps() {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}content/about/`
+    );
+    return { props: { data: res.data } };
+  } catch (error) {
+    console.error("Error fetching about data:", error);
+    // Consider a more sophisticated error handling strategy here
+    return {
+      props: {
+        data: { title: "Error", content: "Could not load the content." },
+      },
+    };
+  }
+}
 export default About;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,7 +16,7 @@ interface ContactProps {
 const Contact = ({ data }: ContactProps) => {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800 p-4">
-      {/* <h1 className="text-4xl font-bold mb-2">{data.title}</h1>
+      <h1 className="text-4xl font-bold mb-2">{data.title}</h1>
       <p className="text-xl mb-4">{data.content}</p>
       <div className="text-lg">
         <p>
@@ -31,24 +31,24 @@ const Contact = ({ data }: ContactProps) => {
             {data.phone}
           </a>
         </p>
-      </div> */}
+      </div>
     </div>
   );
 };
 
-// export async function getStaticProps() {
-//   try {
-//     const res = await axios.get(
-//       `${process.env.NEXT_PUBLIC_API_BASE_URL}content/contact/`
-//     );
-//     return { props: { data: res.data } };
-//   } catch (error) {
-//     console.error("Error fetching contact data:", error);
-//     // Return empty props or default values as a fallback
-//     return {
-//       props: { data: { title: "Error", content: "", email: "", phone: "" } },
-//     };
-//   }
-// }
+export async function getStaticProps() {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}content/contact/`
+    );
+    return { props: { data: res.data } };
+  } catch (error) {
+    console.error("Error fetching contact data:", error);
+    // Return empty props or default values as a fallback
+    return {
+      props: { data: { title: "Error", content: "", email: "", phone: "" } },
+    };
+  }
+}
 
 export default Contact;
